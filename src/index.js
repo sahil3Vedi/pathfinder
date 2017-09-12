ccf729132ab1993cd8e13731f8adde7ac7031713
alan2207 <alan2207@live.com>
2017-09-12 12:32:28 +0200

    initial commit - skeleton of the app created

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+
+
+
+require('./styles/style.css');
+
+import App from './components/App';
+
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
